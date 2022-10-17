09836f91d47d71ee9c3ff5a0c577a948c9ef371d
LEmilBoman <nat14ebo@student.lu.se>
2022-10-17 17:51:35 +0200

    Added comments to vertex files and matrix_standalone_chiral_v4.inc

--- aloha/template_files/cf/LLV1_2.f
+++ aloha/template_files/cf/LLV1_2.f
@@ -5,20 +5,30 @@
       PARAMETER (CI=(0D0,1D0))
       DOUBLE PRECISION RTWO
       PARAMETER (RTWO=2.0D0)
-      INTEGER LEF1,NEXTERNAL,I,J
-      INTEGER LEF2
-      INTEGER MOF1(*),MOF2(*),MO3L(*),MO3R(*)
+      INTEGER NEXTERNAL,I,J
+C     EB: Length of F1 and F2.
+      INTEGER LEF1,LEF2
+C     EB: Left external mothers of F1 and F2.
+      INTEGER MOF1(*),MOF2(*)
+C     EB: Left and right external mothers of V3.
+      INTEGER MO3L(*),MO3R(*)
       COMPLEX*16 COUP
-      COMPLEX*16 F1(*)
-      COMPLEX*16 F2(*)
-      COMPLEX*16 PF1(*)
-      COMPLEX*16 PV3(*)
-      COMPLEX*16 PF2(*)
+C     EB: Vectors carrying innerproducts from previous vertices
+C         for the fermions and the boson.
+      COMPLEX*16 F1(*),F2(*),V3(*)
+C     EB: Complex, 2-component version of 
+C         momentum of F1, F2, and V3.
+C     EB: PV3(3) gives the innerproduct 
+C         for the polarisation factor of V3. 
+      COMPLEX*16 PF1(*),PF2(*),PV3(*)
+C     EB: Real version of F2 momentum.
+      REAL*8 P2(0:3)
+C     EB: Matrices containing all innerproducts for the process.
+C         MSQR contains the square innerproducts.
+C         MANG contains the angle innerproducts.
       COMPLEX*16, DIMENSION(NEXTERNAL,NEXTERNAL) :: MSQR
       COMPLEX*16, DIMENSION(NEXTERNAL,NEXTERNAL) :: MANG
       REAL*8 M2
-      REAL*8 P2(0:3)
-      COMPLEX*16 V3(*)
       REAL*8 W2
       COMPLEX*16 DENOM
       COMPLEX*16 SUMF1
@@ -32,24 +42,26 @@
       DENOM = COUP/(P2(0)**2-P2(1)**2-P2(2)**2-P2(3)**2)
       SUMF1 = 0
       
+C     EB: if F1 is internal.
       IF (LEF1.GT.0) THEN
         LEF2=LEF1+1
         DO I = 1,LEF1
           SUMF1 = SUMF1 + F1(I)*MSQR(ABS(MOF1(I)),ABS(MO3L(1)))
         ENDDO
+C     EB: else F1 is external.
       ELSE
         LEF2=LEF1+2
         SUMF1 = MSQR(ABS(MOF1(1)),ABS(MO3L(1)))
-        
       ENDIF
-      
+
       DO J = 1, LEF2
+C       EB: if external mother of F2 is final.
         IF (MOF2(J).GT.0) THEN
          F2(J) = DENOM*CI*(DSQRT(RTWO)/PV3(3))*SUMF1*MANG(ABS(MO3R(1)),ABS(MOF2(J)))
         ENDIF
+C       EB: if external mother of F2 is initial.
         IF (MOF2(J).LT.0) THEN
          F2(J) = -DENOM*CI*(DSQRT(RTWO)/PV3(3))*SUMF1*MANG(ABS(MO3R(1)),ABS(MOF2(J)))
-        ENDIF
-            
+        ENDIF        
       ENDDO 
       END
--- aloha/template_files/cf/LRV1_0.f
+++ aloha/template_files/cf/LRV1_0.f
@@ -1,26 +1,31 @@
-      SUBROUTINE LRV1_0(F1,LENGTHF1,PF1,MOF1,F2,LENGTHF2,PF2,MOF2,V3,PV3,V3L,V3R,NEXTERNAL,
+      SUBROUTINE LRV1_0(F1,LEF1,PF1,MOF1,F2,LEF2,PF2,MOF2,V3,PV3,MO3L,MO3R,NEXTERNAL,
      & MSQR,MANG,COUP,VERTEX)      
       IMPLICIT NONE
       COMPLEX*16 CI
       PARAMETER (CI=(0D0,1D0))
       DOUBLE PRECISION RTWO
       PARAMETER (RTWO=2.0D0)
-      INTEGER I,J,LENGTHF1,LENGTHF2
-      INTEGER NEXTERNAL,V3L,V3R
+      INTEGER NEXTERNAL,I,J
+C     EB: Length of F1 and F2
+      INTEGER LEF1,LEF2
+C     EB: Left external mothers of F1 and F2.
       INTEGER MOF1(*),MOF2(*)
+C     EB: Left and right external mothers of V3.
+      INTEGER MO3L(*),MO3R(*)
+      COMPLEX*16 COUP
+C     EB: Vectors carrying innerproducts from previous vertices
+C         for the fermions and the boson.
+      COMPLEX*16 F1(*),F2(*),V3(*)
+C     EB: Complex, 2-component version of 
+C         momentum of F1, F2, and V3.
+C     EB: PV3(3) gives the innerproduct 
+C         for the polarisation factor of V3. 
+      COMPLEX*16 PF1(*),PF2(*),PV3(*)
+C     EB: Matrices containing all innerproducts for the process.
+C         MSQR contains the square innerproducts.
+C         MANG contains the angle innerproducts.
       COMPLEX*16, DIMENSION(NEXTERNAL,NEXTERNAL) :: MSQR
       COMPLEX*16, DIMENSION(NEXTERNAL,NEXTERNAL) :: MANG
-      COMPLEX*16 COUP
-      COMPLEX*16 F1(*)
-      COMPLEX*16 F2(*)
-      COMPLEX*16 V3(*)
-      COMPLEX*16 PF1(*)
-      COMPLEX*16 PF2(*)
-      COMPLEX*16 PV3(*)
-      COMPLEX*16 INPRODLMO1F1V3R
-      COMPLEX*16 INPRODLMO2F1V3R
-      COMPLEX*16 INPRODRV3F2
-      COMPLEX*16 POLPRUDLV3V3R
       COMPLEX*16 SUMF1
       COMPLEX*16 SUMF2
       COMPLEX*16 VERTEX
@@ -28,21 +33,25 @@
       SUMF1 = 0
       SUMF2 = 0
       
-      IF (LENGTHF1.GT.0) THEN
-        DO I = 1, LENGTHF1
-          SUMF1 = SUMF1 + F1(I)*MSQR(ABS(MOF1(I)),ABS(V3L))
+C     EB: if F1 is internal.
+      IF (LEF1.GT.0) THEN
+        DO I = 1, LEF1
+          SUMF1 = SUMF1 + F1(I)*MSQR(ABS(MOF1(I)),ABS(MO3L(1)))
         ENDDO
+C     EB: else F1 is external.
       ELSE
-        SUMF1 = MSQR(ABS(MOF1(1)),V3L)
+        SUMF1 = MSQR(ABS(MOF1(1)),ABS(MO3L(1)))
       ENDIF
       
-      IF (LENGTHF2.GT.0) THEN
-        DO J = 1, LENGTHF2
-          SUMF2 = SUMF2 + F2(J)*MANG(ABS(V3R),ABS(MOF2(J)))
+C     EB: if F2 is internal.
+      IF (LEF2.GT.0) THEN
+        DO J = 1, LEF2
+          SUMF2 = SUMF2 + F2(J)*MANG(ABS(MO3R(1)),ABS(MOF2(J)))
         ENDDO
+C     EB: else F2 is external.
       ELSE
-        SUMF2 = MANG(ABS(V3R),ABS(MOF2(1)))       
+        SUMF2 = MANG(ABS(MO3R(1)),ABS(MOF2(1)))       
       ENDIF
-        
+      
       VERTEX = -COUP * (DSQRT(RTWO)/PV3(3)) * SUMF1 * SUMF2 
       END
