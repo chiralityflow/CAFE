c8c5812ba76d95b5ac698e501e4f20f0f5750722
LEmilBoman <nat14ebo@student.lu.se>
2023-06-14 09:44:50 +0200

    version for massive time tests

--- GenProc.sh
+++ GenProc.sh
@@ -5,10 +5,10 @@ co_dir=$(pwd)
 #############################################
 cd $co_dir
 # write the process cards
-python  card_write.py
+python3  card_write.py
 
 # get list of cards (processes) to consider
-cList=$(python -c 'import card_write as c; print(c.getCList())')
+cList=$(python3 -c 'import card_write as c; print(c.getCList())')
 
 # echo "$cList"
 # proc=$(cat $cList)
@@ -32,9 +32,10 @@ while [ -s $cList ]; do
   cd $co_dir
 
   # generate the process
-  printf "\nTesting time to generate files"
+  #printf "\nTesting time to generate files"
   # time (./bin/mg5 cards/$cardUsed > log; cd ${co_dir}/${dir}; caffeinate -i make check > log)
-  time ./bin/mg5_amc cards/$cardUsed > log; 
+  #time ./bin/mg5_aMC cards/$cardUsed > log; 
+  { time ./bin/mg5_aMC cards/$cardUsed > log ; } 2>>makeTime
 
   # go into all SubProcess folders (P1_...). For each folder run the subprocess
   cd $co_dir/$dir
@@ -43,27 +44,33 @@ while [ -s $cList ]; do
     
     # tell user which process we are considering
     export d
-    python $co_dir/write_proc_nicely.py
+    python3 $co_dir/write_proc_nicely.py
 
     # compile subprocess
-    printf "\nTesting time to compile subprocess"
-    time make check > log
+    #printf "\nTesting time to compile subprocess"
+    #time make check > log
+    { time make check > log ; } 2>> $co_dir/makeTime
 
     # run subprocess
-    printf "\nTesting time to evaluate subprocess"
-    time ./check | cat > log
+    #printf "\nTesting time to evaluate subprocess"
+    # time ./check | cat > log
     # for some reason mac monterey doesn't always write to file, so quick workaround
     # was to pipe in cat first. Quicker is just to write to file so uncomment below instead if on Linux
-    # time ./check > log
-    tail -2 log
+    #time ./check  > log
+    { time ./check  > log ; } 2>> $co_dir/runTime
+    #tail -2 runTime 
+    #tail -2 log
+    rm log
 
   done
 
   # remove process from list
   cd $co_dir
   # To run on Mac
-  ghead -n -1 $cList > temp
+  #ghead -n -1 $cList > temp
   # To run on Linux
-  # head -n -1 $cList > temp
+  head -n -1 $cList > temp
   mv temp $cList  
+  python3 timeTest.py
+  rm runTime makeTime
 done
